088fd0adbf2521400532bef691da927333d76a3a
Dipti Mehra <diptimehra345@gmail.com>
2026-08-18 07:22:50 +0530

    LC-search-insert-position [Runtime: N/A; Memory: N/A]

--- array/search-insert-position.cpp
+++ array/search-insert-position.cpp
@@ -2,11 +2,21 @@ class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
         int n = nums.size();
-        for(int i=0;i<n;i++){
-            if(nums[i] >=target){
-                return i;
+        int low=0;
+        int high =n-1;
+        int pos=n;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target){
+                return mid;
+            }else if(nums[mid]>target){
+                pos=mid;
+                high = mid-1;
+            }else{
+                low=mid+1;
             }
         }
-        return n;
+
+        return pos;
     }
 };
